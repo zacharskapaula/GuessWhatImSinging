cabbea7c95a83ff2b9e4d76c500c13d2fe452667
zacharskapaula <84339270+zacharskapaula@users.noreply.github.com>
2022-06-27 15:47:41 +0200

    files

---
+++ GuessWhat/GuessWhat/GuessWhat/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using GuessWhat.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace GuessWhat.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ GuessWhat/GuessWhat/GuessWhat/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using GuessWhat.Models;
+using GuessWhat.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GuessWhat.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
